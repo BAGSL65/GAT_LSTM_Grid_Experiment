70bd58c65b979d84cb55b939e9140b2593984e8b
DESKTOP-0PGGURA\YXF <2334557728@qq.com>
2025-04-24 11:24:34 +0800

    图表生成

--- Draw_Pics.py
+++ Draw_Pics.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 plt.figure(figsize=(10, 6))
 output_dir= "outputs/Pics"
-read_dir= "outputs/GAT_Transformer_Plots"
-save_train_losses_path = os.path.join(read_dir, "GAT_Transformer_train_losses.pkl")
-save_val_losses_path = os.path.join(read_dir, "GAT_Transformer_val_losses.pkl")
+read_dir= "outputs/GAT_LSTM_Plots"
+save_train_losses_path = os.path.join(read_dir, "GAT_LSTM_train_losses.pkl")
+save_val_losses_path = os.path.join(read_dir, "GAT_LSTM_val_losses.pkl")
 
 
 with open(save_train_losses_path, 'rb') as f:
@@ -24,10 +24,10 @@ plt.xlabel('Epoch',fontsize=18)
 plt.ylabel('Loss',fontsize=18)
 plt.tick_params(axis='x', labelsize=15)     # x轴刻度
 plt.tick_params(axis='y', labelsize=15)     # y轴刻度
-plt.title('GAT-Transformer Training and Validation Loss',fontsize=18)
+plt.title('GAT-LSTM Training and Validation Loss',fontsize=18)
 plt.legend(fontsize=18)
 plt.grid()
-loss_curve_path = os.path.join(output_dir, "GAT_Transformer_loss_curve.png")
+loss_curve_path = os.path.join(output_dir, "GAT_LSTM_loss_curve.png")
 plt.savefig(loss_curve_path, dpi=300)
 plt.close()
 logging.info(f"Loss curve saved at {loss_curve_path}")
